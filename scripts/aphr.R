4ea8bfcc3bdcec07b0ab36d7e058a8d8a99537f5
Julian Flowers <julian.flowers12@gmail.com>
2018-06-27 07:12:06 +0100

    new scripts

---
+++ scripts/aphr.R
@@ -0,0 +1,47 @@
+library(pacman)
+p_load(httr, searchConsoleR, rvest, RCurl, XML, urltools, quanteda)
+
+download_pdfs <- function(url){
+  require(rvest)
+  require(dplyr)
+  require(readtext)
+  
+  pdfs- read_html(url) %>%
+    html_nodes("a") %>%
+    html_attr("href") %>%
+    .[grepl("pdf", .)] %>%
+    readtext()
+}
+
+search_term <- "director public health annual report"
+
+url <-URLencode(paste0("https://www.google.com/search?q=", search_term, "&num=",  100))
+aphr <- read_html(url)  
+
+results <- aphr %>%
+  html_nodes("cite") %>%
+  html_text()
+
+aphr <- read_html(url)  
+
+results <- aphr %>%
+  html_nodes("a") %>%
+  html_attr("href") 
+
+aphr_links <- results %>%
+  str_extract_all(., "http.+") %>%
+  str_replace_all(., "&.+", "") %>%
+  .[str_detect(. , "^http")] %>%
+  .[!str_detect(., "webcache|\\+")]
+
+aphr_pdfs <- aphr_links %>%
+  .[str_detect(. , "pdf$")]
+
+aphr_pdfs1 <- readtext(aphr_pdfs[c(1:4, 6:8, 10:11, 13:16, 18:19, 22)]) 
+
+corpus <- corpus(aphr_pdfs1)
+
+corpus
+
+kwic(corpus, "", 5) %>% View()
+
--- scripts/jsna.R
+++ scripts/jsna.R
@@ -3,12 +3,24 @@
 library(pacman)
 p_load(httr, searchConsoleR, rvest, RCurl, XML, urltools, quanteda)
 
+download_pdfs <- function(url){
+  require(rvest)
+  require(dplyr)
+  require(readtext)
+  
+  pdfs- read_html(url) %>%
+    html_nodes("a") %>%
+    html_attr("href") %>%
+    .[grepl("pdf", .)] %>%
+    readtext()
+}
+
 search_term <- "jsna"
 
 url <-URLencode(paste0("https://www.google.com/search?q=", search_term, "&num=",  100))
-jsna <- read_html(url)  
+aphr <- read_html(url)  
 
-results <- jsna %>%
+results <- aphr %>%
   html_nodes("cite") %>%
   html_text()
 
@@ -30,11 +42,7 @@ results_all[1]
 
 camb_jsna <- paste0(results_all[1], "published-joint-strategic-needs-assessments" )
 
-camb_jsna_pdfs <- read_html(camb_jsna) %>%
-  html_nodes("a") %>%
-  html_attr("href") %>%
-  .[grepl("pdf", .)] %>%
-  readtext()
+camb_jsna_pdfs <- download_pdfs(camb_jsna)
 
 cambs_jsna_pdfs <- camb_jsna_pdfs %>% 
   separate(doc_id, c("doc_id", "file", "topic"), sep = "/") %>%
